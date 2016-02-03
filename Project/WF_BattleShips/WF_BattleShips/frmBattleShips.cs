648e9ed0c47016311d9cd48572bff3aad2fe1241
LucienCamuglia <lucien.camuglia@gmail.com>
2016-02-03 14:16:28 +0100

    Creation of project

---
+++ Project/WF_BattleShips/WF_BattleShips/frmBattleShips.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Authors : T.IS-E1
+ * Date    : 03.02.2016
+ * Version : 1.0 
+ * */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_BattleShips
+{
+    public partial class frmBattleShips : Form
+    {
+        public frmBattleShips()
+        {
+            InitializeComponent();
+        }
+    }
+}
